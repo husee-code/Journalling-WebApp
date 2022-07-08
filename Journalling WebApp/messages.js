6f7e783abfe2e26b52e24af39ea3de2907afd18d
husee-code <87664267+husee-code@users.noreply.github.com>
2022-07-08 18:25:05 +0300

    Add files via upload

---
+++ Journalling WebApp/messages.js
@@ -0,0 +1,39 @@
+var messages = {"messages":
+	[
+		{
+			"from": "me",
+			"text": "message1",
+			"date": "14:47"
+		},
+		{
+			"from": "me",
+			"text": "Какой-то рандомны текст который я только что набрал. \nМне вообще похуй че тут будет написано просто вот до пизды и все что ты мне сука сделаешь? отсосешь? вперед",
+			"date": "14:48"
+		},
+        {
+			"from": "me",
+			"text": "message3",
+			"date": "14:49"
+		},
+        {
+			"from": "companion",
+			"text": "message1",
+			"date": "14:50"
+		},
+        {
+			"from": "companion",
+			"text": "message1",
+			"date": "14:50"
+		},
+        {
+			"from": "me",
+			"text": "message1",
+			"date": "14:51"
+		},
+        {
+			"from": "companion",
+			"text": "message1",
+			"date": "14:52"
+		},
+	]
+};
